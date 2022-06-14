f44520a2509719f2ee3dd3dbf80080e2aa12a27b
Samandar Turdiev <firsturdiev@gmail.com>
2022-06-14 05:46:43 +0000

    fix

--- src/data/config.js
+++ src/data/config.js
@@ -8,11 +8,11 @@ const config = {
   KNEX_CONFIG: {
     client: "postgresql",
     connection: {
-      host: config.DATABASE_HOST,
-      user: config.DATABASE_USER,
-      password: config.DATABASE_PASSWORD,
-      database: config.DATABASE_NAME,
-      port: config.DATABASE_PORT
+      host: process.env.DATABASE_HOST,
+      user: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      port: process.env.DATABASE_PORT
     }
   },
   DATABASE_HOST: process.env.DATABASE_HOST,
